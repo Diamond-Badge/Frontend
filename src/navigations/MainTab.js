a76562849df57a50ebe59ef7beb7532059c6ba3c
yunva17 <jiyoon40117@gmail.com>
2021-11-16 23:50:20 +0900

    [Fix] 하단 탭 아이콘 수정

--- src/navigations/MainTab.js
+++ src/navigations/MainTab.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { HomeMap, Diary, Calendar, Mypage } from '../screens'
+import { theme } from '../theme';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,23 +13,59 @@ const MainTab = () => {
                 initialRouteName="지도"
                 screenOptions={{
                     keyboardHidesTabBar: true,
+                    tabBarActiveTintColor: theme.bottomtabtint,
+                    headerShown: false,
+                    tabBarStyle:{height:60,}
                 }}
+                
             >
                 <Tab.Screen 
                     name="지도" 
                     component={HomeMap}
+                    options={{
+                        tabBarIcon:({focused}) =>{
+                            return(
+                                <Image
+                                    source ={focused? require("../assets/icons/homemap_active.png"):
+                                    require("../assets/icons/homemap.png")} /> 
+                            )}
+                    }}
                 />
                 <Tab.Screen 
                     name="일기" 
                     component={Diary}
+                    options={{
+                        tabBarIcon:({focused}) =>{
+                            return(
+                                <Image
+                                    source ={focused? require("../assets/icons/diary_active.png"):
+                                    require("../assets/icons/diary.png")}  />
+                            )}
+                    }}
                 />
                 <Tab.Screen 
                     name="달력" 
                     component={Calendar}
+                    options={{
+                        tabBarIcon:({focused}) =>{
+                            return(
+                                <Image
+                                    source ={focused? require("../assets/icons/calendar_active.png"):
+                                    require("../assets/icons/calendar.png")}  />
+                            )}
+                    }}
                 />
                 <Tab.Screen 
                     name="마이페이지" 
                     component={Mypage}
+                    options={{
+                        tabBarIcon:({focused}) =>{
+                            return(
+                                <Image
+                                    source ={focused? require("../assets/icons/mypage_active.png"):
+                                    require("../assets/icons/mypage.png")}  />
+                            )}
+                    }}
                 />
             </Tab.Navigator>
     );
